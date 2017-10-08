5f38071b2c4f6e74c6b74e532c432586e952e516
Vladimír Smitka <vladimir.smitka@lynt.cz>
2017-10-08 18:26:02 +0200

    SMTP settings

--- extras/mu-plugins/lynt-mailfixer.php
+++ extras/mu-plugins/lynt-mailfixer.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Lynt mail fixer
- * Description: Lynt set up correct mail sender
+ * Description: Lynt set up correct mail settings
  * Author:      Vladimir Smitka
  * Author URI:  https://lynt.cz/
  * License:     GNU General Public License v3 or later
@@ -10,8 +10,20 @@
 
 defined( 'ABSPATH' ) or die( 'nothing here' );
 
-//Set Sender to From address 
-add_action('phpmailer_init', 'lynt_customize_mail_headers');   
-function lynt_customize_mail_headers($phpmailer) {
+add_action('phpmailer_init', 'lynt_customize_mail_settings');
+   
+function lynt_customize_mail_settings($phpmailer) {
+    //Set Sender to From address 
     $phpmailer->Sender = $phpmailer->From;
+    
+    //Setup your own SMTP server
+    /*
+    $phpmailer->Host = 'smpt.server';
+    $phpmailer->Port = 465;
+    $phpmailer->SMTPSecure = 'tls';
+    $phpmailer->Username = 'jmeno';
+    $phpmailer->Password = 'heslo';
+    $phpmailer->SMTPAuth = true;
+    $phpmailer->IsSMTP();
+    */
 }
